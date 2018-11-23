41d5f3532ab88ffd479a5c8f31b5122402904ffe
Gabriel Vieira <gluisvieira@gmail.com>
2018-11-23 11:05:48 -0300

    Add deserialization errors

--- riskprofiler/errors.py
+++ riskprofiler/errors.py
@@ -6,3 +6,19 @@ class InvalidRiskScoreOperation(OriginAdvisorError):
 
 class ItemDataKeyNotUnique(OriginAdvisorError):
     "Duplicated item data key"
+
+class MissingKeyDeserializationError(OriginAdvisorError):
+    def __init__(self, key):
+        self.key = key
+    
+    def __str__(self):
+        return 'missing key "{}" in serialized object'.format(self.key)
+
+class WrongKeyTypeDeserializationError(OriginAdvisorError):
+    def __init__(self, key, actual_type, expected_type):
+        self.key = key
+        self.actual_type = actual_type
+        self.expected_type = expected_type
+    
+    def __str__(self):
+        return 'key "{}" in serialized object has type "{}" (expected "{}") '.format(self.key, self.actual_type, self.expected_type)
